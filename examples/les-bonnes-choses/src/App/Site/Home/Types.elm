117dede54196559e4b7f3b5bcf19e352beb1fa95
Matt Bray <mattjbray@gmail.com>
2016-05-30 15:11:48 +0100

    Unused imports.

--- examples/les-bonnes-choses/src/App/Site/Home/Types.elm
+++ examples/les-bonnes-choses/src/App/Site/Home/Types.elm
@@ -1,8 +1,5 @@
 module App.Site.Home.Types exposing (..)
 
-import App.Documents.Types as Documents
-import Prismic.Types as P
-
 
 type alias Model =
     { doc : Maybe String
--- examples/les-bonnes-choses/src/App/Site/Products/Types.elm
+++ examples/les-bonnes-choses/src/App/Site/Products/Types.elm
@@ -1,6 +1,5 @@
 module App.Site.Products.Types exposing (..)
 
-import App.Documents.Types as Documents
 import App.Site.Products.Index.Types as Index
 import App.Site.Products.Product.Types as Product
 
