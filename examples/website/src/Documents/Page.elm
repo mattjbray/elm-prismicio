768d345a3e417e1fdb4a35a805472eae4c8aa43b
Matt Bray <mattjbray@gmail.com>
2017-08-03 11:49:39 +0100

    Unused imports.

--- examples/website/src/Documents/Page.elm
+++ examples/website/src/Documents/Page.elm
@@ -1,24 +1,7 @@
 module Documents.Page exposing (..)
 
 import Documents.Homepage exposing (BodySlice, bodySliceZone)
-import Prismic.Document
-    exposing
-        ( Decoder
-        , ImageViews
-        , Link
-        , StructuredText
-        , decode
-        , group
-        , image
-        , labelledSlice
-        , link
-        , optional
-        , required
-        , slice
-        , sliceZone
-        , structuredText
-        , text
-        )
+import Prismic.Document exposing (Decoder, decode, required)
 
 
 type alias Page =
